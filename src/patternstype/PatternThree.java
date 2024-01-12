c884267b2cac9f91fbdae90c5d945d1115d4561e
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-12 15:58:31 +0530

    third pattern added

--- src/patternstype/PatternThree.java
+++ src/patternstype/PatternThree.java
@@ -1,4 +1,24 @@
 package patternstype;
 
 public class PatternThree {
+
+    public static void main(String[] args) {
+        int n=10;
+
+        for (int i=0; i<n;i++)
+        {
+            for(int j=0; j<n;j++)
+            {
+                if(j==0 || i==(n-1)/2 || j==n-1)
+                {
+                System.out.print("*");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+    }
+}
 }
